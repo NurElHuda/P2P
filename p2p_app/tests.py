fc22f1cde181651990d67fbe13650ccb40bf8069
Nour Tine <tinenourelhouda@gmail.com>
2022-04-06 17:18:59 +0100

    * test: setup of tests

--- p2p_app/tests.py
+++ p2p_app/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+import requests
 
-# Create your tests here.
+from p2p_app.models import Network
+from django.utils.crypto import get_random_string
+
+
+
+class JoinNodeTestCase(TestCase):
+    def setUp(self):
+        Network.objects.create(name=f"Network {get_random_string(8)}")
+
+    def test_node_can_join(self):
+        """
+            Join a node to a network
+            Test that the node's tree is as expected
+            Test that the node's network is as expected
+            Test that the node's attribute is as expected
+        """
+        network = Network.objects.all().order_by("-pk").last()
+
+        node_data = {
+            "name": f"N-{get_random_string(8)}",
+            "host": "127.0.0.1",
+            "port": "0001",
+            "capacity": 1,
+        }
+        res = requests.post(f"127.0.0.1:8000/networks/{network.pk}/connection/")
+        assert res.status_code == 201
